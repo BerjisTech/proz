721b4db6db533416f9b3c69bfe6bec06346fa5be
Benedict <berjistechnologies@gmail.com>
2024-05-20 16:29:56 +0300

    3 piece home layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadChildren: () =>  import('./modules/nliv/nliv.module').then(m => m.NlivModule) }
+    // { path: 'school', loadChildren: () => import('./modules/school/school.module').then(m => m.SchoolModule) },
+    // { path: 'ai', loadChildren: () => import('./modules/ai/ai.module').then(m => m.AiModule) },
+    // { path: 'course', loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule) },
+    // { path: 'courses', loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule) },
+    // { path: 'suppliers', loadChildren: () => import('./modules/supplier/supplier.module').then(m => m.SupplierModule) },
+    // { path: 'forums', loadChildren: () => import('./modules/forum/forum.module').then(m => m.ForumModule) },
+    // { path: 'library', loadChildren: () => import('./modules/library/library.module').then(m => m.LibraryModule) },
+    { path: '', loadChildren: () => import('./modules/nliv/nliv.module').then(m => m.NlivModule) },
+    { path: '**', redirectTo: '404' }
 ];
